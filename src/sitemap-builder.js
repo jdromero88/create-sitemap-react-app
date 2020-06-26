025b38c2306b136b5a15ef7b2c77eb4d574c553c
jdromero88 <jdromerov88@gmail.com>
2020-06-26 02:35:05 -0400

    Updated sitemap-builder

--- src/sitemap-builder.js
+++ src/sitemap-builder.js
@@ -1,12 +1,11 @@
 require("babel-register")({
   presets: ["es2015", "react"]
 });
-
+require('babel-register');
 const router = require('./Routes').default;
 const Sitemap = require('react-router-sitemap').default;
-
 (
-    new Sitemap(router)
-        .build('https://my-awesome-site.com')
-        .save('./sitemap.xml')
+	new Sitemap(router)
+    .build("https://mi-awesome-website.com")
+		.save('./sitemap.xml')
 );
