923883e3d076c4a92809e0efc9a39374d5c40170
jdromero88 <jdromerov88@gmail.com>
2020-07-01 12:44:15 -0400

    Fixed route bug

---
+++ router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+export default (
+  <Switch>
+    <Route path='/about' />
+    <Route path='/projects' />
+    <Route path='/contact' />
+  </Switch>
+)
--- sitemap-builder.js
+++ sitemap-builder.js
@@ -1,11 +1,10 @@
 require("babel-register")({
   presets: ["es2015", "react"]
 });
-require('babel-register');
-const router = require('./Routes').default;
+const router = require('./router').default;
 const Sitemap = require('react-router-sitemap').default;
 (
 	new Sitemap(router)
     .build("https://mi-awesome-website.com")
-		.save('./sitemap.xml')
+		.save('./public/sitemap.xml')
 );
